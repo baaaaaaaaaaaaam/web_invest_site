26d9f35c69f1df4e679b9dad76c13b40938b05e9
baaaaaaaaaaaaam <ansgyqja@naver.com>
2020-11-08 23:09:33 +0900

    recently

---
+++ invest_git_upload.sh
@@ -0,0 +1,5 @@
+cd /var/www/html/web/
+  cp -rf `ls ./ | grep -v send_mail.php` /home/test/git/web_invest_site/
+  cp /home/test/crawling.sh /home/test/git/web_invest_site/
+  cp -rf /home/test/log /home/test/git/web_invest_site/ 
+  cp -rf /home/test/invest_git_upload.sh /home/test/git/web_invest_site/
